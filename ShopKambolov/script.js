e639b7cbb7d56d7989d869bc30def5b660cb0a50
Tamikon <81911405+Tamikon@users.noreply.github.com>
2023-12-28 02:27:07 +0300

    Add files via upload

--- ShopKambolov/script.js
+++ ShopKambolov/script.js
@@ -1,37 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     launchForm();
     loadShoes();
 });
 
 let shoes = [
-    {firm: 'Nike', footSize: '38', cost: '3990'},
-    {firm: 'Nike', footSize: '40', cost: '2900'},
-    {firm: 'Nike', footSize: '41', cost: '3500'},
-    {firm: 'Adidas', footSize: '39', cost: '3499'},
-    {firm: 'Adidas', footSize: '42', cost: '5600'},
-    {firm: 'Adidas', footSize: '41', cost: '3300'},
-    {firm: 'BoxFresh', footSize: '38', cost: '3800'},
-    {firm: 'Nike', footSize: '39', cost: '4900'},
-    {firm: 'Nike', footSize: '41', cost: '4990'}
-]
+    { firm: 'Nike', footSize: '38', cost: '3990' },
+    { firm: 'Nike', footSize: '40', cost: '2900' },
+    { firm: 'Nike', footSize: '41', cost: '3500' },
+    { firm: 'Adidas', footSize: '39', cost: '3499' },
+    { firm: 'Adidas', footSize: '42', cost: '5600' },
+    { firm: 'Adidas', footSize: '41', cost: '3300' },
+    { firm: 'BoxFresh', footSize: '38', cost: '3800' },
+    { firm: 'Nike', footSize: '39', cost: '4900' },
+    { firm: 'Nike', footSize: '41', cost: '4990' }
+];
 
-function createShoe(shoe){
-    let cell = createElement("div");
+function createShoe(shoe) {
+    let cell = document.createElement("div");
     cell.className = "cell";
-    cell.InnerHTML = `<h3>Производитель: ${shoe.firm}</h3><p><br>Размер обуви: ${shoe.footSize}</p><br>Цена: ${shoe.cost}</p>`;
+    cell.innerHTML = `<h3>Фирма:<br> ${shoe.firm}</h3><p><br>Размер обуви: ${shoe.footSize}</p><br>Цена: ${shoe.cost}</p>`;
     return cell;
 }
 
-function loadShoes(){
-    let table = document.querySelector("shoesGrid");
+function loadShoes() {
+    let table = document.getElementById("gridForm");
 
     shoes.forEach(shoe => {
         let cell = createShoe(shoe);
         table.appendChild(cell);
-    })
+    });
 }
 
-function setForm(){
+function setForm() {
     let userForm = document.getElementById("userParamsForm");
     let data = new FormData(userForm);
 
@@ -44,10 +44,10 @@ function setForm(){
     localStorage.setItem("userParamsForm", JSON.stringify(profile));
 }
 
-function launchForm(){
+function launchForm() {
     let storedProfile = localStorage.getItem("userParamsForm");
     if (storedProfile) {
-        let  profile = JSON.parse(storedProfile);
+        let profile = JSON.parse(storedProfile);
 
         for (const key in profile) {
             if (Object.hasOwnProperty.call(profile, key)) {
@@ -61,17 +61,21 @@ function filterProducts() {
     const storedProfile = localStorage.getItem("userParamsForm");
     if (storedProfile) {
         let user = JSON.parse(storedProfile);
-        let userFirm = user.firm || user.firm.toLowerCase();
-        let userFootSize = parseInt(user.footSize, 10);
-        let userCost = parseInt(user.cost, 10)
+        let userFirm = user.firm ? user.firm.toLowerCase() : "";
+        let userFootSize = parseFloat(user.footSize) || 0;
+        let userCost = parseFloat(user.cost) || 0;
 
         const shoesGrid = document.getElementById("gridForm");
         shoesGrid.innerHTML = "";
 
         const filteredProducts = shoes.filter(item => {
-            return (!user.firm || item.firm === userFirm) &&
-                    (!user.footSize || item.footSize === userFootSize) &&
-                    (!user.cost || item.cost === userCost);
+            const itemFirm = item.firm.toLowerCase();
+            const itemFootSize = parseFloat(item.footSize);
+            const itemCost = parseFloat(item.cost);
+
+            return (!user.firm || itemFirm === userFirm) &&
+                (!user.footSize || itemFootSize === userFootSize) &&
+                (!user.cost || itemCost === userCost);
         });
 
         filteredProducts.forEach(item => {
@@ -80,8 +84,4 @@ function filterProducts() {
         });
     }
 }
-
-
-
-
 
